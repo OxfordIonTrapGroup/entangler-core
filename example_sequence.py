9d1c491698f9d047c80494490e828df3d2fa3dbd
Chris Ballance <chris.ballance@physics.ox.ac.uk>
2018-06-27 01:19:47 +0100

    sketched out RTIO interface (without timeout / herald events yet)

--- example_sequence.py
+++ example_sequence.py
@@ -21,7 +21,7 @@ entangler.set_cycle_length(1.05*us)
 
 # Set the count patterns that cause the entangler loop to exit
 # Up to 4 patterns can be set.
-entangler.set_heralds([0,1,0,1], [1,0,0,1], [0,1,1,0], [1,0,1,0])
+entangler.set_heralds(0b0101, 0b1010, 0b0110, 0b1010)
 
 
 while True:
@@ -29,11 +29,10 @@ while True:
 
     # Enable the override switches
     entangler.set_config(enable=True)
-    delay(10*ns)
     _422sigma.sw.on()
     _1092.sw.on()
     _422pulsed.sw.on()
-    # DMA from stops here
+    # Above can all be in DMA if required
 
     # Run entangler core until sucess, or 200*us has elapsed
     # (finishes at the end of the current cycles)
@@ -46,11 +45,6 @@ while True:
 
     if r:
         break
-
-
-# Get herald
-entangler.get_herald_pattern()
-
 
 # Number of entanglement cycles run since last enable
 n = entangler.get_n_cycles()
---
+++ phy.py
@@ -0,0 +1,83 @@
+from migen import *
+
+from artiq.gateware.rtio import rtlink
+from ??? import EntanglerCore
+
+
+class Entangler(Module):
+    def __init__(self, eem_pads, phy_apd1, phy_apd2, phy_422pulse):
+        """eem_pads: pads for inter-Kasli EEM
+        phy_apd1, phy_apd2: phys for APDs!
+        phy_422pulse: phy for 422 pulse input
+        """
+        self.rtlink = rtlink.Interface(
+            rtlink.OInterface(
+                data_width=32,
+                address_width=5,
+                enable_replace=False),
+            rtlink.IInterface(
+                data_width=14,
+                timestamped=False)
+            )
+
+        # # #
+
+        read_en = self.rtlink.o.address[4]
+        write_timings = Signal()
+        self.comb += [
+            self.rtlink.o.busy.eq(0),
+            write_timings.eq(self.rtlink.o.address[4:3] == 1),
+        ]
+
+        self.submodules.core = EntanglerCore(eem_pads, [phy_apd1, phy_apd2],
+                            phy_422pulse)
+
+        output_t_starts = [Signal(14) for _ in range(8)]
+        output_t_ends = [Signal(14) for _ in range(8)]
+
+
+        self.sync.rio += [
+            If(write_timings & self.rtlink.o.stb, 
+                    output_t_starts[self.rtlink.o.address[2:]].eq(self.rtlink.o.data[13:]),
+                    output_t_ends[self.rtlink.o.address[2:]].eq(self.rtlink.o.data[29:16])
+                ),
+            If(self.rtlink.o.address==0 & self.rtlink.o.stb,
+                    # Write config
+                ),
+            If(self.rtlink.o.address==1 & self.rtlink.o.stb,
+                    # Pulse run flag
+                    # Write timeout reg
+                ),
+            If(self.rtlink.o.address==2 & self.rtlink.o.stb,
+                    # Write cycle length
+                ),
+            If(self.rtlink.o.address==3 & self.rtlink.o.stb,
+                    # Write herald pattern
+                ),
+        ]
+
+
+        read = Signal()
+        read_timings = Signal()
+        read_addr = Signal()
+
+        input_timestamps = [Signal(14) for _ in range(5)]
+
+        self.sync.rio += [
+                If(read,
+                    read.eq(0)
+                ),
+                If(self.rtlink.o.stb,
+                    read.eq(read_en),
+                    read_timings.eq(self.rtlink.o.address[4:3] == 3),
+                    read_addr.eq(self.rtlink.o.address[2:]),
+                )
+        ]
+
+        self.comb += [
+                self.rtlink.i.stb.eq(read),
+                self.rtlink.i.data.eq(
+                    Mux(read_timings,
+                        input_timestamps[read_addr],
+                        status if read_addr==0 else n_cycles))
+        ]
