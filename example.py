b4253a81dee92e37b78e085bd008a39ac037ad89
Chris Ballance <chris.ballance@physics.ox.ac.uk>
2018-06-29 11:50:45 +0100

    remove outdated example

--- example.py
+++
@@ -1,7 +0,0 @@
-
-
-e = Entangler(pads, [phy_apd_1, phy_apd_2])
-
-mem = rtservo.RTServoMem(iir_p, su)
-target.submodules += mem
-target.rtio_channels.append(rtio.Channel.from_phy(mem, ififo_depth=4))
